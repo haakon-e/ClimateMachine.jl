967574f33b22c91c2127494087bfaa6a292e7330
Akshay Sridhar <asridhar@caltech.edu>
2020-06-19 14:40:02 -0700

    Check variable scope

--- experiments/AtmosLES/baroclinicwave.jl
+++ experiments/AtmosLES/baroclinicwave.jl
@@ -13,7 +13,7 @@ using ClimateMachine.Thermodynamics
 using ClimateMachine.VariableTemplates
 
 using CLIMAParameters
-using CLIMAParameters.Planet: e_int_v0, grav, day, cp_d, cv_d, R_d, γ_d, grav, Ω, planet_radius
+using CLIMAParameters.Planet: e_int_v0, grav, day, cp_d, cv_d, R_d, grav, planet_radius
 struct EarthParameterSet <: AbstractEarthParameterSet end
 const param_set = EarthParameterSet()
 
@@ -34,7 +34,7 @@ using Logging
 Initialisation helper for baroclinic-wave (channel flow) test case for iterative
 determination of η = p/pₛ coordinate for given z-altitude. 
 """
-function baroclinic_instability_cube(FT, param_set)
+function baroclinic_instability_cube(FT, (x,y,z), param_set)
 
   ### Unpack CLIMAParameters
   _planet_radius = FT(planet_radius(param_set))
@@ -48,7 +48,7 @@ function baroclinic_instability_cube(FT, param_set)
   Lp2   = Lp*Lp              
   xc    = FT(2e6)              ## Streamwise center of perturbation
   yc    = FT(25e6)             ## Spanwise center of perturbation
-  gamma_lapse = FT(5//1000)    ## Γ Lapse Rate
+  gamma_lapse = FT(5/1000)    ## Γ Lapse Rate
   Ω     = FT(7.292e-5)         ## Rotation rate [rad/s]
   f0    = 2Ω/sqrt(2)           ## 
   beta0 = f0/_planet_radius    ##  
@@ -58,24 +58,33 @@ function baroclinic_instability_cube(FT, param_set)
   u0    = FT(35)
   Ly    = FT(6e6)
   T0    = FT(288)
-  T_ref = T0                   ## TODO: What is the correct reference temperature. Choose SURFACE TEMP
+  T_ref = T0                   
   x0    = FT(2e7)
   p00   = FT(1e5)              ## Surface pressure
+
   #Step 1: Get current coordinate value by unpacking nodal coordinates from aux state
-  eta = eps(FT)
+  eta = FT(1e-7)
+  global geo_phi = FT(1.0)
+  global temp = FT(1.0)
+  @show(geo_phi, temp, "Before iterations")
+
+  (geo_phi, temp, η) = baroclinic_iterations(geo_phi, temp, η, 
+
   for n = 1:100 ## Begin convergence loop
-    (geo_phi, temp) = baroclinic_instability_cube_functions(y,eta,f0,beta0,u0,T0,gamma_lapse, gravity)
+    baroclinic_instability_cube_functions!(geo_phi,temp,y,eta,f0,beta0,u0,T0,gamma_lapse, gravity, R_gas, cp)
     num  = -gravity*z + geo_phi
     den  = -R_gas/(eta)*temp
     deta = num/den
     eta  = eta - deta
-    if (abs(deta) <= eps(FT))
-      break
-    else
-      @warn "Baroclinic Instability Initialisation: η iterations did not converge."
-      break
+    if (abs(deta) <= FT(1e-10))
+        break
+    elseif (abs(deta) > FT(1e-10)) && n<100
+        continue
+    elseif (abs(deta) > FT(1e-10)) && n==100
+      @error "Baroclinic Instability Initialisation: η iterations did not converge."
     end
   end ## End convergence loop
+  @show(eta, "After iterations")
   eta = min(eta,FT(1))
   eta = max(eta,FT(0))
   logeta = log(eta)
@@ -83,7 +92,7 @@ function baroclinic_instability_cube(FT, param_set)
   press = p00*eta
   theta = T *(p00/press)^(R_gas/cp)
   rho = press/(R_gas*T)
-  thetaref = Tref * (1 - gamma_lapse*z/T0)^(-gravity/(cp*gamma_lapse))
+  thetaref = T_ref * (1 - gamma_lapse*z/T0)^(-gravity/(cp*gamma_lapse))
   rhoref = p00/(T0*R_gas) * (1 - gamma_lapse*z/T0)^(gravity/(R_gas*gamma_lapse) - 1)
 
   ### Balanced Flow
@@ -99,11 +108,11 @@ function baroclinic_instability_cube(FT, param_set)
   return rho, u, du, T
 end 
 
-function baroclinic_instability_cube_functions(y,eta,f0,beta0,u0,T0,gamma_lapse,gravity)
+function baroclinic_instability_cube_functions!(geo_phi, temp,y,eta,f0,beta0,u0,T0,gamma_lapse,gravity, R_gas, cp)
   FT    = eltype(y)
   b     = FT(2)
   Ly    = FT(6e6)  
-  y0    = FT(Ly//2)
+  y0    = FT(Ly/2)
   b2    = b*b
   #Get Mean Temperature
   exp1  = R_gas*gamma_lapse/gravity
@@ -119,8 +128,8 @@ function baroclinic_instability_cube_functions(y,eta,f0,beta0,u0,T0,gamma_lapse,
   phi_prime=FT(0.5)*u0*(fac1 + fac2)
   geo_phi = phimean + phi_prime*fac3*logeta
   temp = Tmean + phi_prime/R_gas*fac3*(2/b2*logeta*logeta - 1)
-
-  return (geo_phi, temp)
+  @show(geo_phi, temp, "Inside BICF")
+  return nothing
 end 
 
 function init_baroclinicwave!(bl, state, aux, (x, y, z), t)
@@ -130,17 +139,17 @@ function init_baroclinicwave!(bl, state, aux, (x, y, z), t)
     gravity = FT(grav(param_set))
 
     ### Unpack initial conditions (solved by iterating for η)
-    (rho, u, du, T) = baroclinic_instability_cube(FT, param_set)
+    (rho, u, du, T) = baroclinic_instability_cube(FT, (x,y,z), param_set)
     
     ### Primitive variables
     u⃗ = SVector{3,FT}(u,0,0)
-    e_kin = 1/2*sum(abs2.(u⃗))
+    e_kin = FT(1/2)*sum(abs2.(u⃗))
     e_pot = gravity * z
 
     ### Assign state variables for initial condition
     state.ρ = rho
-    state.ρu = rho * u⃗
-    state.ρe = rho*(param_set, e_kin, e_pot, T)
+    state.ρu = rho .* u⃗
+    state.ρe = rho * total_energy(param_set, e_kin, e_pot, T)
 end
 
 function config_baroclinicwave(FT, N, resolution, xmax, ymax, zmax)
@@ -187,19 +196,38 @@ function config_baroclinicwave(FT, N, resolution, xmax, ymax, zmax)
 end
 
 function config_diagnostics(driver_config)
+
+    boundaries = [
+        FT(0.0) FT(0.0) FT(0.0)
+        FT(4e7) FT(6e6) FT(30e3)
+    ]
+    resolution = (FT(100e3), FT(75e3), FT(1250)) 
+    interpol = ClimateMachine.InterpolationConfiguration(
+        driver_config,
+        boundaries,
+        resolution,
+    )
+
     default_dgngrp = setup_atmos_default_diagnostics(
         AtmosLESConfigType(),
-        "2500steps",
+        "1shours",
         driver_config.name,
     )
     core_dgngrp = setup_atmos_core_diagnostics(
         AtmosLESConfigType(),
-        "2500steps",
+        "1shours",
         driver_config.name,
+    )
+    dump_dgngrp = setup_dump_state_and_aux_diagnostics(
+        AtmosLESConfigType(),
+        "1shours",
+        driver_config.name,
+        interpol = interpol,
     )
     return ClimateMachine.DiagnosticsConfiguration([
         default_dgngrp,
         core_dgngrp,
+        dump_dgngrp,
     ])
 end
 
@@ -209,15 +237,16 @@ function main()
     # DG polynomial order
     N = 4
     # Domain resolution and size
-    Δh = FT(100)
-    Δv = FT(40)
+    Δx = FT(100000)
+    Δy = FT(75000)
+    Δz = FT(1250)
 
-    resolution = (Δh, Δh, Δv)
+    resolution = (Δx, Δy, Δz)
 
     # Prescribe domain parameters
-    xmax = FT(40000)
-    ymax = FT(6000)
-    zmax = FT(30000)
+    xmax = FT(4e7)
+    ymax = FT(6e6)
+    zmax = FT(30e3)
 
     t0 = FT(0)
 
